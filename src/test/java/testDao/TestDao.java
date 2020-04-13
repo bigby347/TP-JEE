784a393d2f6f6606a79729cd264ac923fc80f138
alexandre <alexandreputzu@gmail.com>
2020-04-13 16:49:34 +0200

    modif pom.xml add junit 5

--- src/test/java/testDao/TestDao.java
+++ src/test/java/testDao/TestDao.java
@@ -1,4 +1,29 @@
 package testDao;
 
+import org.junit.jupiter.api.*;
+
 public class TestDao {
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+
+    }
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void name() {
+    }
 }
